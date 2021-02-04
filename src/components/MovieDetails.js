86cfe67f08add7e961a4b32556e83a21c0b095a0
Lucas Merchant <lbmerchant93@gmail.com>
2021-02-04 16:15:50 -0700

    Build out basic Component structure for MovieDetails

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const MovieDetails = () => {
-  return (
-    <div>
-      This is the movie details page
-    </div>
-  )
+class MovieDetails extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movie: {}
+    }
+  }
+
+  componentDidMount = () => {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <img src='' alt='' />
+        <h3 className='movieTitle'>Movie Title</h3>
+        <h3 className='rating'>Rating</h3>
+        <h4 className='releaseDate'>Release Date</h4>
+      </div>
+    )
+  }
 }
 
 export default MovieDetails;
