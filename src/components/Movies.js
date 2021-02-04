dda9b8460f15b65b05c7d9b3002070c30d4a3fcf
kellydinneen <kgdinneen@gmail.com>
2021-02-04 10:23:51 -0500

    Style scroll buttons

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Poster from './Poster';
+import scrollLeftButton from './scroll-left.png';
+import scrollRightButton from './scroll-right.png';
 import './Movies.css';
 
 const Movies = ({movies}) => {
@@ -27,11 +29,11 @@ const Movies = ({movies}) => {
 
   return (
     <div className='movies'>
-      <button id="slideLeft" type="button" onClick={scrollLeft}>Slide left</button>
+      <img className="scrollButton" onClick={scrollRight} src={scrollLeftButton} onClick={scrollLeft}/>
       <div className='movies-container' id='container' ref={scroller}>
         {moviePosters}
       </div>
-      <button id="slideRight" type="button" onClick={scrollRight}>Slide right</button>
+      <img className="scrollButton" onClick={scrollRight} src={scrollRightButton} onClick={scrollRight}/>
     </div>
   )
 }
