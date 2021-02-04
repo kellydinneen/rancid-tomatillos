3b3c732c4c1c4b5ec85d87a0676ed0a3c0069d73
kellydinneen <kgdinneen@gmail.com>
2021-02-04 10:05:55 -0500

    Clean up comments

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,42 +2,6 @@ import React from 'react';
 import Poster from './Poster';
 import './Movies.css';
 
-// class Movies extends React.Component {
-//   constructor({movies}) {
-//     super({movies})
-//     this.movies = {movies};
-//     this.scroller = React.createRef();
-//   }
-//
-//   getPosters() {
-//     return this.movies.map(movie => {
-//       return (
-//         <Poster
-//           image={movie.poster_path}
-//           id={movie.id}
-//           key={movie.id}
-//         />
-//       )
-//     })
-//   }
-//
-//   scrollRight() {
-//     this.scroller.current.scrollLeft -= 20;
-//   }
-//
-//   render() {
-//     return (
-//       <div className='movies'>
-//         <button id="slideLeft" type="button">Slide left</button>
-//         <div className='movies-container' ref={this.scroller}>
-//           {this.getPosters}
-//         </div>
-//         <button id="slideRight" type="button" onClick={this.scrollRight}>Slide right</button>
-//       </div>
-//     )
-//   }
-// }
-
 const Movies = ({movies}) => {
 
   const scroller = React.useRef();
