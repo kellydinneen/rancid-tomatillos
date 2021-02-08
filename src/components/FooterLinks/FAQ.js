8247de9d42bd3695e212f32c66d1592516f2e8bf
kellydinneen <kgdinneen@gmail.com>
2021-02-07 19:39:13 -0700

    Fix

--- src/components/FooterLinks/FAQ.js
+++ src/components/FooterLinks/FAQ.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const FAQ = () => {
   return (
-    <h2>PAGE COMING SOON! SORRY!</h2>
+    <div>
+      <h2>How can I leave a movie review?</h2>
+      <p>At the moment, we gather only preexisting movie review data and there is no way for our users to leave a review through our site. This is a feature we may add to Rancid Tomatillos in the future.</p>
+      <h2>Why is your selection of movies so limited?</h2>
+      <p></p>
+    </div>
   )
 }
 
