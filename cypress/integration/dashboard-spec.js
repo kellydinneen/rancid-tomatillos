4ee02bb58ff5ea53bca3ba4fad7014f9749c52f4
Lucas Merchant <lbmerchant93@gmail.com>
2021-02-08 19:18:35 -0700

    Test for title in header. Test for Top Rated section to contain the section title and the top three movie titles

--- cypress/integration/dashboard-spec.js
+++ cypress/integration/dashboard-spec.js
@@ -3,21 +3,26 @@ describe('Dashboard UI', () => {
     cy.visit('http://localhost:3000');
   });
 
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
+  it('Should be able to visit the page and render the title', () => {
+    cy.get('header').contains('Rancid Tomatillos')
   });
 
-  it('Should be able to visit the page and render the correct elements', () => {
-      cy.contains('Rancid Tomatillos')
-      cy.contains('Top Rated')
-      cy.get('footer').contains('About')
-  });
+  it('Should render a section for the Top Rated movies that has the top three movies\'s titles and ratings', () => {
+    cy.contains('Top Rated')
+    cy.get('.top-three-section').contains('h3', '7')
+    cy.get('.top-three-section').contains('h3', '7')
+    cy.get('.top-three-section').contains('h3', '6.8')
+    cy.get('.top-three-section').contains('h4', 'Peninsula')
+    cy.get('.top-three-section').contains('h4', 'Cats & Dogs 3: Paws Unite')
+    cy.get('.top-three-section').contains('h4', 'The King of Staten Island')
+  })
 
-  it('Should be able to visit the page and render the correct elements', () => {
-      cy.get('.movies-container').children().first('link').click()
-      cy.url()
-      cy.contains('minutes')
-      cy.get('.featuredMovieData').contains('Money Plane')
-  });
+
+  // it('Should be able to visit the page and render the correct elements', () => {
+  //     cy.get('.movies-container').children().first('link').click()
+  //     cy.url()
+  //     cy.contains('minutes')
+  //     cy.get('.featuredMovieData').contains('Money Plane')
+  // });
 
 });
