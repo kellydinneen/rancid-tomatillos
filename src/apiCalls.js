026c38ba8e50c7825cb4ad65e9bb6c2f46957c9a
kellydinneen <kgdinneen@gmail.com>
2021-02-08 18:45:09 -0700

    Remove comments

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,22 +7,3 @@ export const fetchMovieData = (endpoint) => {
         return res.json()
       }})
   }
-
-// export const fetchSpecificMovieDetails = (id) => {
-//   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-//   .then(res => {
-//     if (!res.ok) {
-//         return `${res.status} error. Sorry! Something went wrong! Try again later or go to Contact Us to contact the developers with questions!`
-//       } else {
-//         return res.json()
-//       }})
-//   }
-//  export const fetchTrailerKey = (id) => {
-//    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
-//      .then(res => {
-//        if (!res.ok) {
-//            return `${res.status} error. Sorry! Something went wrong! Try again later or go to Contact Us to contact the developers with questions!`
-//          } else {
-//            return res.json()
-//          }})
-//   }
