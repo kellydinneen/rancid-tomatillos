c6bfbb6db84cf44ddc695f777bb140f562455b9c
kellydinneen <kgdinneen@gmail.com>
2021-02-09 16:56:35 -0700

    Test for header, footer, and home navigation

--- cypress/integration/movie-details-spec.js
+++ cypress/integration/movie-details-spec.js
@@ -4,4 +4,31 @@ describe('Movie Details UI', () => {
     cy.get('.movies-container').children().first('link').click()
   });
 
+  it('Should have header and footer', () => {
+      cy.get('footer').contains('About')
+      cy.get('footer').contains('FAQ')
+      cy.get('header').contains('Rancid Tomatillos')
+      cy.get('header').contains('button')
+  });
+
+  it('Should return to home after clicking home button', () => {
+      cy.get('header').get('button').click()
+      cy.contains('Top Rated')
+  });
+
+  it('Should return to home after clicking home button', () => {
+      cy.get('header').get('button').click()
+      cy.contains('Top Rated')
+  });
+
+  it('Should return to home after clicking home button', () => {
+      cy.get('trailerContainer').contains('movieBackdrop')
+  });
+
+  it('Should return to home after clicking home button', () => {
+      cy.get('viewTrailerBtn').click()
+      cy.get('trailer').click().screenshot()
+  });
+
+
 });
