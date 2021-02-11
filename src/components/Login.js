52c220f4d63aad337caaafea93bca88f2af5f19c
Lucas Merchant <lbmerchant93@gmail.com>
2021-02-10 20:42:54 -0700

    Add email input field

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,32 @@
-import React, Component from 'react';
+import React, { Component } from 'react';
 class Login extends Component {
   constructor(){
     super()
     this.state = {
-
+      email: '',
+      password: '',
+      errorMsg: '',
+      redirect: false
     }
   }
 
   render() {
+    const { email, password, errorMsg, redirect } = this.state;
+
     return (
-      <div>THIS IS THE LOGIN PAGE</div>
+      <form>
+        <h2 className="form-title">Please Login</h2>
+        <label>
+          Email
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            value={email}
+          />
+          </label>
+
+      </form>
     )
   }
 
