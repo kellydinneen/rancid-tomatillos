969b19a1a1736d313f8385d2699c9faa09d50045
Lucas Merchant <lbmerchant93@gmail.com>
2021-02-12 19:20:55 -0700

    Pass test for first movie in all movies to take user to Movie Details page

--- cypress/integration/dashboard-spec.js
+++ cypress/integration/dashboard-spec.js
@@ -28,9 +28,9 @@ describe('Dashboard UI', () => {
     cy.get('.top-three-section > #1').contains('7')
     cy.get('.top-three-section > #2').contains('7')
     cy.get('.top-three-section > #3').contains('6.8')
-    cy.get('.top-movie-title').contains('Peninsula')
-    cy.get('.top-movie-title').contains('Cats & Dogs 3: Paws Unite')
-    cy.get('.top-movie-title').contains('The King of Staten Island')
+    cy.get('.top-movie-title1').contains('Cats & Dogs 3: Paws Unite')
+    cy.get('.top-movie-title2').contains('Peninsula')
+    cy.get('.top-movie-title3').contains('The King of Staten Island')
   });
 
   it('Should be able to click the rating or title for the top rated movie and visit the correct Movie Details page', () => {
@@ -38,26 +38,18 @@ describe('Dashboard UI', () => {
     cy.url().should('include', 'movie-details/Cats&Dogs3:PawsUnite')
   });
 
-  it.only('Should be able to click the title of the top rated movie and visit the correct Movie Details page', () => {
+  it('Should be able to click the title of the top rated movie and visit the correct Movie Details page', () => {
     cy.get('.top-movie-title1').click()
     cy.url().should('include', 'movie-details/Cats&Dogs3:PawsUnite')
-  });
-
-  it.skip('Should be able to click the rating or title for the third top rated movie and visit the correct Movie Details page', () => {
-    cy.get('#3 > h3').click()
-    cy.url().should('include', 'movie-details/TheKingofStatenIsland')
-    cy.visit('http://localhost:3000')
-    cy.get('#3 > h4').click()
-    cy.url().should('include', 'movie-details/TheKingofStatenIsland')
   });
 
   it('Should have a section containing all of the movie posters', () => {
     cy.get('.movies-container a').should('have.length', '8')
   });
 
-  it.skip('Should be able to click the first child in the movie container and go to the url with the title of the movie as the endpoint', () => {
+  it('Should be able to click the first child in the movie container and go to the url with the title of the movie as the endpoint', () => {
     cy.get('.movies-container').children().first().click()
-    cy.url().should('include', 'MoneyPlane')
+    cy.url().should('include', 'Cats&Dogs3:PawsUnite')
   })
 
   it('Should have a left scroll image that has an alt stating it is the left scroll image', () => {
