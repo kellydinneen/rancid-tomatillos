5d2fb640b130aa7df53b83c9cc1bd1ba1fe0b264
kellydinneen <kgdinneen@gmail.com>
2021-02-14 07:37:18 -0700

    Merge with feature/dashboard testing

--- cypress/integration/dashboard-spec.js
+++ cypress/integration/dashboard-spec.js
@@ -1,5 +1,15 @@
 describe('Dashboard UI', () => {
   beforeEach(() => {
+    cy.fixture('testMovies.json')
+      .then((testMovies) => {cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+        statusCode: 200,
+        body: testMovies
+      })})
+    cy.fixture('testMovies.json')
+      .then((testMovie) => {cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/726739', {
+        statusCode: 200,
+        body: testMovie
+      })})
     cy.visit('http://localhost:3000');
   });
 
@@ -7,68 +17,58 @@ describe('Dashboard UI', () => {
     cy.get('header').contains('Rancid Tomatillos').should('be.visible')
   });
 
+  it('Should have a button to the Login page', () => {
+    cy.get('.login-button').contains('Log in')
+      .click()
+    cy.url().should('include', 'login')
+  });
+
   it('Should render a section for the Top Rated movies that has the top three movies\'s titles and ratings', () => {
     cy.contains('Top Rated')
     cy.get('.top-three-section > #1').contains('7')
     cy.get('.top-three-section > #2').contains('7')
     cy.get('.top-three-section > #3').contains('6.8')
-    cy.get('.top-three-section > #1').contains('h4', 'Peninsula')
-    cy.get('.top-three-section > #2').contains('h4', 'Cats & Dogs 3: Paws Unite')
-    cy.get('.top-three-section > #3').contains('h4', 'The King of Staten Island')
-  })
+    cy.get('.top-movie-title1').contains('Cats & Dogs 3: Paws Unite')
+    cy.get('.top-movie-title2').contains('Peninsula')
+    cy.get('.top-movie-title3').contains('The King of Staten Island')
+  });
 
   it('Should be able to click the rating or title for the top rated movie and visit the correct Movie Details page', () => {
-    cy.get('#1 > h3').click()
-    cy.url().should('include', 'movie-details/Peninsula')
-    cy.visit('http://localhost:3000')
-    cy.get('#1 > h4').click()
-    cy.url().should('include', 'movie-details/Peninsula')
-  })
-
-  it('Should be able to click the rating or title for the second top rated movie and visit the correct Movie Details page', () => {
-    cy.get('#2 > h3').click()
+    cy.get('.top-movie-rating1').click()
     cy.url().should('include', 'movie-details/Cats&Dogs3:PawsUnite')
-    cy.visit('http://localhost:3000')
-    cy.get('#2 > h4').click()
-    cy.url().should('include', 'movie-details/Cats&Dogs3:PawsUnite')
-  })
+  });
 
-  it('Should be able to click the rating or title for the third top rated movie and visit the correct Movie Details page', () => {
-    cy.get('#3 > h3').click()
-    cy.url().should('include', 'movie-details/TheKingofStatenIsland')
-    cy.visit('http://localhost:3000')
-    cy.get('#3 > h4').click()
-    cy.url().should('include', 'movie-details/TheKingofStatenIsland')
-  })
+  it('Should be able to click the title of the top rated movie and visit the correct Movie Details page', () => {
+    cy.get('.top-movie-title1').click()
+    cy.url().should('include', 'movie-details/Cats&Dogs3:PawsUnite')
+  });
 
   it('Should have a section containing all of the movie posters', () => {
-    cy.get('.movies-container a').should('have.length', '40')
-  })
+    cy.get('.movies-container a').should('have.length', '8')
+  });
 
-  // it('Should have a link for each movie in the section that holds all of the movie posters', () => {
-  //   cy.get('.movies-container').children()
-  //     .each((element) => {
-  //       element.children()
-  //     })
-  // })
+  it('Should be able to click the first child in the movie container and go to the url with the title of the movie as the endpoint', () => {
+    cy.get('.movies-container').children().first().click()
+    cy.url().should('include', 'Cats&Dogs3:PawsUnite')
+  })
 
   it('Should have a left scroll image that has an alt stating it is the left scroll image', () => {
     cy.get('.scrollButtonLeft').should('have.attr', 'alt', 'Scroll Left Arrow')
-  })
+  });
 
   it('Should have a right scroll image that has an alt stating it is the right scroll image', () => {
     cy.get('.scrollButtonRight').should('have.attr', 'alt', 'Scroll Right Arrow')
-  })
+  });
 
   it('Should scroll to the right in the section containing all the movie posters.', () => {
     cy.get('.movies-container').scrollTo('right')
-  })
+  });
 
   it('Should render a footer that contains an About, FAQ, and Contact Us links', () => {
     cy.get('.about').contains('About')
     cy.get('.faq').contains('FAQ')
     cy.get('.contact-us').contains('Contact Us')
-  })
+  });
 
   it('Should be able to click the footer link About and visit the corresponding page', () => {
     cy.get('.about').click()
@@ -85,4 +85,33 @@ describe('Dashboard UI', () => {
     cy.url().should('include', '/contact-us')
   });
 
+});
+
+describe('Dashboard UI 404 Error', () => {
+  beforeEach(() => {
+      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+        statusCode: 404
+      })
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Should display the error number and a message when something goes wrong with the api call to retrieve all of the movies is not ok', () => {
+    cy.get('main').contains("404 error. Sorry! Something went wrong! Try again later or go to Contact Us to contact the developers with questions!")
+  })
+});
+
+describe('Dashboard UI Errors', () => {
+  beforeEach(() => {
+    cy.fixture('testMovies.json')
+      .then((result) => {
+        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+        statusCode: 500,
+        body: result.error500
+      })})
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Should display the error number and a message when something goes wrong with the api call to retrieve all of the movies is not ok', () => {
+    cy.get('main').contains("500 error. Sorry! Something went wrong! Try again later or go to Contact Us to contact the developers with questions!")
+  })
 });
---
+++ src/components/App.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import { Route, Switch, Link, NavLink } from "react-router-dom";
+import MovieDetails from './MovieDetails';
+import Home from './Home';
+import Login from './Login';
+import About from './FooterLinks/About';
+import FAQ from './FooterLinks/FAQ';
+import ContactUs from './FooterLinks/ContactUs';
+import homeButton from '../Images/home.png';
+import { fetchMovieData } from '../apiCalls'
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      isLoading: true,
+      errorMsg: null,
+      atHome: window.location.pathname === '/' ? true : false,
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    fetchMovieData('movies')
+        .then(result =>{
+          if (!result.movies) {
+            this.setState({
+              isLoading: false,
+              errorMsg: result
+            })
+          } else {
+            this.setState({
+                movies: result.movies,
+                isLoading: false
+              })
+          }})
+  }
+
+  goHome = () => {
+    this.setState({ atHome: true })
+  }
+
+  leaveHome = () => {
+    this.setState({ atHome: false })
+  }
+
+  logIn = (user) => {
+      this.setState({ user: user })
+    }
+
+  logOut = () => {
+      this.setState({ user: null })
+    }
+
+
+  render() {
+    const {movies, isLoading, errorMsg, atHome, user} = this.state;
+
+    return (
+      <>
+      <header>
+        <nav className="header-content">
+          <h1>
+            <NavLink to={{
+              pathname:'/'
+            }}  className="site-title" onClick={this.goHome}>Rancid<br/> Tomatillos
+            </NavLink>
+          </h1>
+            <NavLink to={{
+              pathname:'/login'
+            }} className='login-link' onClick={this.leaveHome}>
+              {!user && <button className='login-button'>Log in</button>}
+              {user && <button className='logout-button'>Log out</button>}
+            </NavLink>
+          {!atHome &&
+            <NavLink to={{
+              pathname:'/'
+              }}>
+              <img src={homeButton} alt="home button" className='home-button' onClick={this.goHome}/>
+            </NavLink>
+          }
+        </nav>
+        <section className="gradient"></section>
+      </header>
+        <Switch>
+          <Route path='/' exact render={() => <Home
+            errorMsg={errorMsg}
+            isLoading={isLoading}
+            movies={movies}
+            leaveHome={this.leaveHome}
+            />} />
+          <Route path='/movie-details/:title' exact component={MovieDetails} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/profile' exact component={Profile} />
+          <Route path='/about' exact component={About} />
+          <Route path='/faq' exact component={FAQ} />
+          <Route path='/contact-us' exact component={ContactUs} />
+          <Route path='/' render={() => <div>404</div>} />
+        </Switch>
+        <footer>
+          <section className="gradient"></section>
+          <nav className="footer-links">
+            <NavLink className="about footer-link" to='/about' onClick={this.leaveHome}>About</NavLink>
+            <NavLink className="faq footer-link" to='/faq' onClick={this.leaveHome}>FAQ</NavLink>
+            <NavLink className="contact-us footer-link" to='/contact-us' onClick={this.leaveHome}>Contact Us</NavLink>
+          </nav>
+        </footer>
+      </>
+    )
+  }
+
+}
+
+export default App;
